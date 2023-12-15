906f8c152ae4f8bf89be90ebc5faeab9318cb77f
Jaehyun Lee <gisholic@gmail.com>
2023-12-15 19:17:44 +0900

    Update jandi.js

--- jandi.js
+++ jandi.js
@@ -1,35 +1,33 @@
-const axios = require('axios');
 const dotenv = require('dotenv');
-const { expand } = require('dotenv-expand');
+dotenv.config();
+Object.assign(process.env, dotenv.config({ path: `.env.${process.env.NODE_ENV}` }));
 
-expand(dotenv.config({ path: `.env.${process.env.NODE_ENV}` }));
-expand(dotenv.config({ path: '.env' }));
+async function webhook() {
+  const url = process.env.JANDI_WEBHOOK_URL;
+  if (url) {
+    const text = `[${process.env.NODE_ENV}] ${process.argv[2] || process.env.body || ''}`.trim();
+    const message = (process.argv[3] || process.env.message)?.replace(/\r?\n|\r/g, ' ');
+    if (!text && !message) return;
 
-if (process.env.JANDI_WEBHOOK_URL) {
-  const body = `[${process.env.NODE_ENV}] ${process.argv[2] || process.env.body || ''}`.trim();
-  const message = (process.argv[3] || process.env.message)?.replace(/\r?\n|\r/g, ' ');
-  if (!body && !message) return;
-
-  axios
-    .post(process.env.JANDI_WEBHOOK_URL, {
+    const res = await fetch(url, {
+      method: 'POST',
       headers: {
         Accept: 'application/vnd.tosslab.jandi-v2+json',
-        'Content-Type': 'application/json',
+        'Content-Type': 'application/json; charset=UTF-8',
       },
-      body: body,
-      connectColor: process.env.NODE_ENV === 'production' ? '#1BFA22' : '#FAC11B',
-      connectInfo: [
-        {
-          title: process.env.title,
-          description: message,
-          imageUrl: process.env.imageUrl, // req.headers.referer,
-        },
-      ],
-    })
-    .then(function (response) {
-      console.log('Webhook sent successfully.');
-    })
-    .catch(function (error) {
-      console.log(error);
+      body: JSON.stringify({
+        body: text,
+        connectColor: process.env.NODE_ENV === 'production' ? '#1BFA22' : '#FAC11B',
+        connectInfo: [
+          {
+            title: process.env.title,
+            description: message,
+            imageUrl: process.env.imageUrl, // req.headers.referer,
+          },
+        ],
+      }),
     });
+    return await res.json();
+  }
 }
+webhook().then(res => console.log('Webhook sent successfully.'));
